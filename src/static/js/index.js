9787b9890d0c9d219651349038545e4915343dda
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2023-11-13 14:58:40 -0300

    criacao de sections

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,1 +1,14 @@
-console.log("teste");
+/*
+ * Slider
+ */
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});

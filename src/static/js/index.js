6b8e7199f0d9acb3101073c80b0c09f0d56f372a
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-02-20 19:14:46 -0300

    correcao em componentes e adicionando section de previsao

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -12,3 +12,44 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/*
+ * Predict Age
+ */
+
+async function getPredict(name) {
+  const response = await fetch(`https://api.agify.io/?name=${name}`);
+  const result = await response.json();
+
+  return result;
+}
+
+async function applyingPrediction() {
+  const nameUser = document.getElementById("user-name").value;
+
+  if (nameUser) {
+    const predictResult = await getPredict(nameUser);
+
+    document.querySelector(
+      ".container-predict-age-component-result"
+    ).innerHTML = `
+        <p>
+          Apostamos que <span>${predictResult.name}</span> tem <span>${predictResult.age}</span> anos!
+        </p>
+      `;
+  }
+}
+
+document
+  .querySelector(".container-predict-age-component-input #user-name")
+  .addEventListener("keypress", async function (e) {
+    if (e.key === "Enter") {
+      applyingPrediction();
+    }
+  });
+
+document
+  .querySelector(".container-predict-age-component-input button")
+  .addEventListener("click", async function (e) {
+    applyingPrediction();
+  });
